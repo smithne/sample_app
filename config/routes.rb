749bef202d1678c179d4d69ac6aa8597a2b93d16
Nick Smith <smithne@uw.edu>
2019-05-17 20:32:57 -0700

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
+  
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
 
   root 'application#hello'
 
