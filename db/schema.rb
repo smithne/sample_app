e107faee3ab710e583113fea600bf537fa5bb7d2
Nick Smith <smithne@uw.edu>
2019-05-31 16:01:21 -0700

    Set email column of users table to be a unique index

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190531034433) do
+ActiveRecord::Schema.define(version: 20190531230001) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
