9eb527d3b60ee07e6ec67f70620955fb5214946c
Nick Smith <smithne@uw.edu>
2019-06-08 09:18:04 -0700

    Add initialize session controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,19 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
+  # static pages
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  # user routes
+  resources :users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
-  resources :users
+  # sessions (login/logout)
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
 end
