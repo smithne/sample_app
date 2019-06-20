a9576c823bd2682e431f3497e9f8f872bd2e8952
Nick Smith <smithne@uw.edu>
2019-06-19 21:06:44 -0700

    Prevent editing & updating another user's profile

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -50,6 +50,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
+    follow_redirect!
     get edit_user_path(@user)
     assert flash.empty?
     assert_redirected_to root_url
@@ -57,6 +58,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "should redirect update when logged in as wrong user" do
     log_in_as(@other_user)
+    follow_redirect!
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
