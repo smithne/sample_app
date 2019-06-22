1a0c7e81846dadd336041453f270051d822464f9
Nick Smith <smithne@uw.edu>
2019-06-21 20:35:03 -0700

    Add test to ensure link to users index only appears after logging in

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,5 +16,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "link to users page appears when logged in" do
+    get root_path
+    assert_select "a[href=?]", users_path, count: 0
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
   end
 end
