98cad0f92c27e6c40416d458d926cd77baa01c44
Nick Smith <smithne@uw.edu>
2019-06-22 16:07:50 -0700

    Add method and tests for deleting users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,6 +5,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user       = users(:michael)
     @other_user = users(:archer)
+    @third_user = users(:lana)
   end
 
   test "should get new" do
@@ -15,6 +16,48 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect when not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                        user: { password: 'password',
+                                password_confirmation: 'password',
+                                admin: true}
+    }
+    @other_user.reload
+    assert_not @other_user.admin?
+  end
+
+  test "should not allow non-admins to delete users" do
+    log_in_as(@other_user)
+    delete user_path(@third_user)
+    @third_user.reload
+    assert_not @third_user.nil?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  test "should allow admin to delete users" do
+    log_in_as(@user)
+    assert_difference 'User.count', -1 do
+      delete user_path(@other_user)
+    end
+    assert_redirected_to users_path
   end
 
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,16 +2,27 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @admin = users(:michael)
+    @non_admin = users(:archer)
   end
 
-  test "index includes pagination" do
-    log_in_as(@user)
+  test "non-admin index includes pagination but not delete" do
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', 2
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
   end
+
+  test "non-admin index does not include delete buttons" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+
 end
