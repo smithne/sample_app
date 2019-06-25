95591fa39c4fdc28455bc6bd542bfde00a36cecc
Nick Smith <smithne@uw.edu>
2019-06-24 18:59:19 -0700

    Add account activation edit path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  # account activation routes
+  resources :account_activations, only: [:edit]
   
 end
