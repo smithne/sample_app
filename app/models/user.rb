5d70e0c63dcd119a40387d358d3a9e4f45fd2b1b
Nick Smith <smithne@uw.edu>
2019-06-24 19:16:27 -0700

    Add account activation attributes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
     has_secure_password
 
-    before_save { self.email = email.downcase }
+    before_save :downcase_email
+    before_create :create_activation_digest
 
     # validations
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -37,5 +38,16 @@ class User < ApplicationRecord
     def authenticated?(remember_token)
         return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    private
+
+    def create_activation_digest
+        self.activation_token = User.new_token
+        self.activation_digest = User.digest(activation_token)
+    end
+
+    def downcase_email
+        self.email.downcase!
     end
 end
